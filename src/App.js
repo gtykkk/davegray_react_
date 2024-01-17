bb7869b71717acba8ed16106043ba2add798e338
gtykkk <qwez0498@naver.com>
2024-01-17 15:05:27 +0900

    fetch 사용

--- src/App.js
+++ src/App.js
@@ -4,33 +4,85 @@ import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 import Footer from './Footer';
 import { useState, useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || [])
+  const API_URL = 'http://localhost:3500/items'
+
+  const [items, setItems] = useState([])
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
-  // []를 붙이게 되면 어떤 조건이 실행되면 작동한다
+  // []를 붙이게 되면 어떤 조건이 실행되는 방법을 의미
   useEffect(() => {
-    localStorage.setItem('shoppinglist', JSON.stringify(items))
-  }, [items])
+    // 한번만 실행되면 되기 때문에, useEffect 안에서 사용
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        if (!response.ok) throw Error('Did not received expected data')
+        const listItems = await response.json()
+        setItems(listItems)
+        setFetchError(null)
+      } catch (err) {
+        setFetchError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    // 2초 후에 데이터 로딩
+    setTimeout(() => {
+      (async () => await fetchItems())()
+    }, 2000)
+  }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
     const myNewItem = { id, checked: false, item }
     const listItems = [...items, myNewItem]
     setItems(listItems)
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+
+    const result = await apiRequest(API_URL, postOptions)
+    if (result) setFetchError(result)
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
     setItems(listItems)
+
+    const myItem = listItems.filter(item => item.id === id)
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked })
+    }
+
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, updateOptions)
+
+    if (result) setFetchError(result)
   }
 
-
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id)
     setItems(listItems)
+
+    const deleteOptions = { method: 'DELETE' }
+    const reqUrl = `${API_URL}/${id}`
+
+    const result = await apiRequest(reqUrl, deleteOptions)
+    if (result) setFetchError(result)
   }
 
   const handleSubmit = (e) => {
@@ -57,12 +109,18 @@ function App() {
         setSearch={setSearch}
         handleSearch={handleSearch}
       />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        setItems={setItems}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading &&
+          <Content
+            items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+            setItems={setItems}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        }
+      </main>
       <Footer
         length={items.length}
       />
